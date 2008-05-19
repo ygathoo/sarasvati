e67059b3e0ffa2c99885226cdcdb212e1b2f8ed1
paul <paul@2a3b5a84-814d-0410-8c37-a95beea58766>
2008-05-19 21:26:54 +0000

    Move painting code into graph nodes Date: 2008-05-14 17:45:04

---
+++ java/trunk/src/org/codemonk/wf/visual/TaskGraphTreeNode.java
@@ -0,0 +1,40 @@
+package org.codemonk.wf.visual;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import org.codemonk.wf.db.NodeRef;
+import org.codemonk.wf.test.NodeTask;
+
+public class TaskGraphTreeNode extends GraphTreeNode
+{
+  public TaskGraphTreeNode( GraphTreeNode parent, NodeRef node )
+  {
+    super( parent, node );
+    color = new Color( 102, 152, 102 );
+  }
+
+  @Override
+  public void paintNode( Graphics g )
+  {
+    g.setColor( Color.blue );
+
+    int maxRadius = NodeDrawConfig.getMaxNodeRadius();
+
+    g.draw3DRect( originX - maxRadius, originY - maxRadius, maxRadius << 1, maxRadius << 1, false );
+
+    NodeTask nodeTask = (NodeTask)node.getNode();
+
+    String taskName = nodeTask.getTaskName();
+
+    int width = g.getFontMetrics().stringWidth( taskName );
+    int height = g.getFontMetrics().getHeight();
+    g.drawString( taskName, originX - (width >> 1), originY - (height >> 1) );
+  }
+
+  @Override
+  public int getOffset ()
+  {
+    return NodeDrawConfig.getMaxNodeRadius();
+  }
+}
