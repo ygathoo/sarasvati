a1221fddc8208b0f44a3f15d6daebe6271ba10c6
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2008-06-24 03:47:35 +0000

    Going to retag 0.1.0

--- haskell/tags/v0.1.0/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rm -r out/*
-
-ghc -Wall -fno-warn-name-shadowing -fglasgow-exts --make -isrc -o bin/wf-mem-example -hidir out -odir out src/Workflow/Example/MemoryWfEngineExample.hs
-
-ghc -Wall -fno-warn-name-shadowing -fglasgow-exts --make -isrc -o bin/wf-db-example -hidir out -odir out src/Workflow/Example/DatabaseWfEngineExample.hs
-
-ghc -Wall -fno-warn-name-shadowing -fglasgow-exts --make -isrc -o bin/wf-db-loader-example -hidir out -odir out src/Workflow/Example/DatabaseLoaderExample.hs
--- haskell/tags/v0.1.0/gen-parser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-happy -g lang/guard.y -o src/Workflow/GuardLang.hs
