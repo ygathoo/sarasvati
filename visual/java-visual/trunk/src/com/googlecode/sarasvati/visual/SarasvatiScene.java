015fc97fd9632cce007efe14b26920939fac7c0a
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2008-09-05 02:00:25 +0000

    Add in code from wf-java and graph-test projects

---
+++ visual/java-visual/trunk/src/com/googlecode/sarasvati/visual/SarasvatiScene.java
@@ -0,0 +1,42 @@
+/*
+    This file is part of Sarasvati.
+
+    Sarasvati is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Sarasvati is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Sarasvati.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2008 Paul Lorenz
+*/
+package com.googlecode.sarasvati.visual;
+
+import javax.swing.JLabel;
+
+import org.netbeans.api.visual.widget.ComponentWidget;
+import org.netbeans.api.visual.widget.Widget;
+
+import com.googlecode.sarasvati.Arc;
+import com.googlecode.sarasvati.Node;
+
+public class SarasvatiScene extends GraphSceneImpl<Node, Arc>
+{
+  @Override
+  protected Widget widgetForNode (Node node)
+  {
+    if ( "task".equals( node.getType() ) )
+    {
+      //return new ComponentWidget( this, new TaskComponent( node ) );
+      return new ComponentWidget( this, new JLabel( new TaskIcon( node ) ) );
+    }
+
+    return super.widgetForNode(node);
+  }
+}
