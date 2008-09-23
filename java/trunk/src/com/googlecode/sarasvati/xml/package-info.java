1b7409ce4713c1db3647a19a387271b415bb97a0
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2008-09-23 00:42:56 +0000

    Update process definition xsd so no prefix is required

--- java/trunk/src/com/googlecode/sarasvati/xml/package-info.java
+++ java/trunk/src/com/googlecode/sarasvati/xml/package-info.java
@@ -1,5 +1,6 @@
 
-@XmlSchema( namespace="http://sarasvati.googlecode.com/ProcessDefinition" )
+@XmlSchema( namespace="http://sarasvati.googlecode.com/ProcessDefinition",
+            elementFormDefault=javax.xml.bind.annotation.XmlNsForm.QUALIFIED )
 package com.googlecode.sarasvati.xml;
 
 import javax.xml.bind.annotation.XmlSchema;
