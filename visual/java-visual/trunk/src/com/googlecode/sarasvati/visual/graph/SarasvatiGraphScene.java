870acbfc61518ee3e019b788a2aff33db37ef4a7
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2008-10-22 03:31:36 +0000

    more export up to graphsceneimpl level use Scene2Image, so we don't have to write out image twice

--- visual/java-visual/trunk/src/com/googlecode/sarasvati/visual/graph/SarasvatiGraphScene.java
+++ visual/java-visual/trunk/src/com/googlecode/sarasvati/visual/graph/SarasvatiGraphScene.java
@@ -19,27 +19,12 @@
 package com.googlecode.sarasvati.visual.graph;
 
 import java.awt.Component;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
-import javax.imageio.ImageIO;
-
-import org.netbeans.api.visual.export.SceneExporter;
-import org.netbeans.api.visual.export.WidgetPolygonalCoordinates;
-import org.netbeans.api.visual.export.SceneExporter.ImageType;
-import org.netbeans.api.visual.export.SceneExporter.ZoomType;
 import org.netbeans.api.visual.widget.ComponentWidget;
 import org.netbeans.api.visual.widget.Widget;
 
 import com.googlecode.sarasvati.Arc;
 import com.googlecode.sarasvati.Node;
-import com.googlecode.sarasvati.adapter.Function;
-import com.googlecode.sarasvati.visual.ConvertUtil;
 import com.googlecode.sarasvati.visual.GraphSceneImpl;
 
 public class SarasvatiGraphScene extends GraphSceneImpl<Node, Arc>
@@ -48,42 +33,5 @@ public class SarasvatiGraphScene extends GraphSceneImpl<Node, Arc>
   protected Widget widgetForNode (Node node)
   {
     return new ComponentWidget( this, node.getAdaptor( Component.class ) );
-  }
-
-  public void export (String imageFile, StringBuilder buf, Function<String, Widget> hrefMapper, Function<String, Widget> titleMapper )
-    throws IOException
-  {
-    List<WidgetPolygonalCoordinates> coords =
-      SceneExporter.createImageMap( getScene(),
-                                    new File( imageFile),
-                                    ImageType.PNG,
-                                    ZoomType.ACTUAL_SIZE,
-                                    false,
-                                    false,
-                                    100,
-                                    this.getView().getWidth(),
-                                    this.getView().getHeight(),
-                                    0 );
-
-    for ( WidgetPolygonalCoordinates coord : coords )
-    {
-      buf.append( "<area shape=\"poly\" coords=\"" );
-      ConvertUtil.appendPolygon( coord.getPolygon(), buf );
-      buf.append( "\" " );
-      buf.append( hrefMapper.apply( coord.getWidget() ) );
-      buf.append( " " );
-      buf.append( titleMapper.apply( coord.getWidget() ) );
-      buf.append( ">\n" );
-    }
-
-    Rectangle bounds = getScene().getPreferredBounds();
-    BufferedImage image = new BufferedImage( bounds.width, bounds.height, BufferedImage.TYPE_4BYTE_ABGR );
-
-    Graphics2D g = image.createGraphics();
-    g.setRenderingHint( RenderingHints.KEY_ANTIALIASING, // Anti-alias!
-                        RenderingHints.VALUE_ANTIALIAS_ON );
-
-    paint( g );
-    ImageIO.write( image, "gif", new File( "/home/paul/tmp/image.gif" ) );
   }
 }
