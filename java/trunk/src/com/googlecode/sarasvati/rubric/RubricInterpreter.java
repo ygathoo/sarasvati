6c4e5de11283bfff412f86cb10888e3728b2b183
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2008-12-05 05:35:34 +0000

    update javadocs in preparation for beta3 release

--- java/trunk/src/com/googlecode/sarasvati/rubric/RubricInterpreter.java
+++ java/trunk/src/com/googlecode/sarasvati/rubric/RubricInterpreter.java
@@ -27,10 +27,42 @@ import com.googlecode.sarasvati.rubric.env.RubricEnv;
 import com.googlecode.sarasvati.rubric.lang.ErrorReportingRubricParser;
 import com.googlecode.sarasvati.rubric.lang.RubricLexer;
 import com.googlecode.sarasvati.rubric.lang.RubricStmt;
+import com.googlecode.sarasvati.rubric.visitor.RubricVisitor;
 
+/**
+ * Rubric is a simple rules language. It as the following grammer
+ *
+ * <pre>
+ * STMT = 'if' EXPR 'then' STMT 'else' STMT
+ *      | Accept | Discard | Skip | Skip ARCNAME
+ *      | unquoteStringWithNoSpaces
+ *      | "quoted string"
+ *      | '(' dateFunction ')'
+ *      | '(' NUMBER (hour|hours|day|days|week|weeks) (before|after) dateFunction ')'
+ *
+ * EXPR = predicate
+ *      | predicate 'or' EXPR
+ *      | predicate 'and' EXPR
+ *      | 'not' EXPR
+ *      | '(' EXPR ')'
+ * </pre>
+ *
+ * @author Paul Lorenz
+ */
 public class RubricInterpreter
 {
-  public static RubricStmt compile (String rubricStatement)
+  /**
+   * Takes a Rubric program and returns a compiled version in the form of
+   * an Abstract Syntax Tree (AST). The AST can be executed/evaluated with
+   * a call to {@link RubricStmt#eval(RubricEnv)} or the AST can be traversed
+   * using a {@link RubricVisitor} with call to
+   * {@link RubricStmt#traverse(RubricVisitor)}.
+   *
+   * @param rubricStatement A String containing a Rubric program
+   * @throws RubricException Thrown if the program is not valid.
+   * @return A RubricStmt, which is the root of the compiled AST.
+   */
+  public static RubricStmt compile (String rubricStatement) throws RubricException
   {
     RubricLexer lexer = new RubricLexer( new ANTLRStringStream( rubricStatement ) );
 
@@ -55,11 +87,5 @@ public class RubricInterpreter
       re.printStackTrace();
       return null;
     }
-  }
-
-  public static Object eval (String rubricStatement, RubricEnv env)
-  {
-    RubricStmt stmt = compile( rubricStatement );
-    return stmt.eval(  env );
   }
 }
