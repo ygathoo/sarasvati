63ac0f481f4fc59c43901b3656eac839884734bc
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2008-12-09 02:57:01 +0000

    tag 1.0.0-beta3 release

---
+++ java/tags/v1.0.0-beta3/src/com/googlecode/sarasvati/rubric/lang/RubricStmt.java
@@ -0,0 +1,30 @@
+/*
+    This file is part of Sarasvati.
+
+    Sarasvati is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Sarasvati is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Sarasvati.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2008 Paul Lorenz
+*/
+
+package com.googlecode.sarasvati.rubric.lang;
+
+import com.googlecode.sarasvati.rubric.env.RubricEnv;
+import com.googlecode.sarasvati.rubric.visitor.RubricVisitor;
+
+public interface RubricStmt
+{
+  Object eval (RubricEnv env);
+
+  void traverse (RubricVisitor visitor);
+}
