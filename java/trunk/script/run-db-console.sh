28fcd293505e43be646c31109e4bc8f5f072d33e
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-01-16 03:34:38 +0000

    update scripts to build examples with ant

--- java/trunk/script/run-db-console.sh
+++ java/trunk/script/run-db-console.sh
@@ -1,2 +1,3 @@
-java -cp lib/antlr-2.7.6.jar:lib/ejb3-persistence.jar:lib/hibernate-commons-annotations-3.3.1.jar:lib/hibernate-3.2.6.jar:lib/hibernate-3.2.5.jar:lib/commons-collections-3.2.jar:lib/cglib.jar:lib/hibernate-annotations-3.3.1.jar:lib/commons-logging.jar:lib/asm.jar:lib/jboss-j2ee.jar:lib/postgresql-8.3-603.jdbc4.jar:lib/dom4j.jar:classes/ com.googlecode.sarasvati.example.db.DbConsole
+ant compile-examples
+java -cp lib/antlr-3.1.1.jar:lib/ejb3-persistence.jar:lib/hibernate-commons-annotations-3.3.1.jar:lib/hibernate-3.2.6.jar:lib/hibernate-3.2.5.jar:lib/commons-collections-3.2.jar:lib/cglib.jar:lib/hibernate-annotations-3.3.1.jar:lib/commons-logging.jar:lib/asm.jar:lib/jboss-j2ee.jar:lib/postgresql-8.3-603.jdbc4.jar:lib/dom4j.jar:dist/examples:dist/build:dist/lang com.googlecode.sarasvati.example.db.DbConsole
 
--- java/trunk/script/run-mem-console.sh
+++ java/trunk/script/run-mem-console.sh
@@ -1,2 +1,2 @@
-java -cp classes com.googlecode.sarasvati.example.mem.MemConsole
-
+ant compile-examples
+java -cp dist/examples:dist/build:dist/lang:lib/antlr-3.1.1.jar com.googlecode.sarasvati.example.mem.MemConsole
