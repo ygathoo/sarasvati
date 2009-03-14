b16517b0214e7b37098523fe575d5a2932e665b0
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-03-14 20:27:28 +0000

    remove, so I can recreate from trunk

--- java/branches/templates/src/com/googlecode/sarasvati/xml/package-info.java
+++
@@ -1,7 +0,0 @@
-
-@XmlSchema( namespace="http://sarasvati.googlecode.com/ProcessDefinition",
-            elementFormDefault=javax.xml.bind.annotation.XmlNsForm.QUALIFIED )
-package com.googlecode.sarasvati.xml;
-
-import javax.xml.bind.annotation.XmlSchema;
-
