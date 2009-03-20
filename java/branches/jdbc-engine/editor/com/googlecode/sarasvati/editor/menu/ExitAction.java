dfc23cf588647a08c04d23942ca8e98ad3fcb02e
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-03-20 00:17:25 +0000

    feature branch for JDBC engine

---
+++ java/branches/jdbc-engine/editor/com/googlecode/sarasvati/editor/menu/ExitAction.java
@@ -0,0 +1,50 @@
+/*
+    This file is part of Sarasvati.
+
+    Sarasvati is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Sarasvati is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Sarasvati.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2008 Paul Lorenz
+*/
+package com.googlecode.sarasvati.editor.menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+
+import com.googlecode.sarasvati.editor.GraphEditor;
+
+public class ExitAction extends AbstractAction
+{
+  private static final long serialVersionUID = 1L;
+
+  protected GraphEditor editor;
+
+  public ExitAction (GraphEditor editor)
+  {
+    super( "Exit" );
+    this.editor = editor;
+
+    putValue( Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke( KeyEvent.VK_Q, KeyEvent.CTRL_MASK ) );
+    putValue( Action.MNEMONIC_KEY, KeyEvent.VK_X );
+  }
+
+  @Override
+  public void actionPerformed (ActionEvent e)
+  {
+    System.exit( 0 );
+  }
+}
