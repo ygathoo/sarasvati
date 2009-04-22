60a5eda9f3f259cedc1d4d61593a9fe1d254122f
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-04-22 04:08:01 +0000

    some demo stuff

---
+++ java/trunk/examples/com/googlecode/sarasvati/example/demo/Demo.java
@@ -0,0 +1,18 @@
+package com.googlecode.sarasvati.example.demo;
+
+import com.googlecode.sarasvati.Graph;
+import com.googlecode.sarasvati.GraphProcess;
+import com.googlecode.sarasvati.mem.MemEngine;
+
+public class Demo
+{
+  @SuppressWarnings("unused")
+  public static void main (String[] args) throws Exception
+  {
+    MemEngine engine = new MemEngine();
+    engine.addNodeType( "task", TaskNode.class );
+    engine.getLoader().load( "/home/paul/workspace/wf-common/test-wf/demo-example.wf.xml" );
+    Graph graph = engine.getRepository().getLatestGraph( "order" );
+    GraphProcess p = engine.startProcess( graph );
+  }
+}
