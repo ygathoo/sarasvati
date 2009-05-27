ea1782ea0b1a13191a60e2259f921af18083f034
chungonn <chungonn@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-05-27 13:50:40 +0000

    Added HibNodeType and @NodeType classes

---
+++ java/trunk/src/com/googlecode/sarasvati/hib/HibNodeType.java
@@ -0,0 +1,78 @@
+/*
+    This file is part of Sarasvati.
+
+    Sarasvati is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Sarasvati is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Sarasvati.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2008 Paul Lorenz
+*/
+package com.googlecode.sarasvati.hib;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * 
+ * @author chungonn
+ *
+ */
+@Entity
+@Table(name = "wf_node_type")
+public class HibNodeType
+{
+  @Id
+  protected String    id;
+
+  private String      description;
+
+  @OneToOne()
+  @JoinColumn(name = "Behaviour")
+  private HibNodeType behaviour;
+
+  public HibNodeType ()
+  {
+  }
+
+  public HibNodeType (String id, String description)
+  {
+    this.id = id;
+    this.description = description;
+    this.behaviour = this;
+  }
+
+  public HibNodeType (String id, String description, HibNodeType type)
+  {
+    this.id = id;
+    this.description = description;
+    this.behaviour = type;
+  }
+
+  public String getId ()
+  {
+    return id;
+  }
+
+  public HibNodeType getBehaviour ()
+  {
+    return behaviour;
+  }
+
+  public String getDescription ()
+  {
+    return description;
+  }
+
+}
