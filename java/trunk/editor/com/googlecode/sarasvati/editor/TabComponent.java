dbbe68373bfc5465fefb2a72122216f3e1a26c7f
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-07-14 03:09:08 +0000

    Make tabs closeable. Can't get the background color to be anything other than bleaugh.

---
+++ java/trunk/editor/com/googlecode/sarasvati/editor/TabComponent.java
@@ -0,0 +1,68 @@
+/*
+    This file is part of Sarasvati.
+
+    Sarasvati is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Sarasvati is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Sarasvati.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2008-2009 Paul Lorenz
+*/
+package com.googlecode.sarasvati.editor;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.border.EtchedBorder;
+
+public class TabComponent extends JPanel
+{
+  private static final long serialVersionUID = 1L;
+
+  private final JLabel label;
+
+  public TabComponent (final JTabbedPane pane, final String labelText)
+  {
+    label = new JLabel( labelText );
+    label.setBorder( BorderFactory.createEmptyBorder( 0, 10, 0, 5 ) );
+
+    setLayout( new BorderLayout() );
+    setOpaque( false );
+    add( label, BorderLayout.CENTER );
+
+    JButton closeButton = new JButton ( "X" );
+    closeButton.setBorder( BorderFactory.createEtchedBorder( EtchedBorder.LOWERED ) );
+    closeButton.setFont( Font.decode( "sans bold 11" ) );
+
+    closeButton.addActionListener( new ActionListener()
+    {
+      @Override
+      public void actionPerformed (ActionEvent e)
+      {
+        GraphEditor.getInstance().closeTab( pane.indexOfTabComponent( TabComponent.this ) );
+      }
+    });
+
+    add( closeButton, BorderLayout.EAST );
+  }
+
+  public void setLabelText (final String text)
+  {
+    label.setText( text );
+  }
+}
