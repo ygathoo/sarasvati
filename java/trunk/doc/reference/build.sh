8559316f9e4d57eb984959aca1488e2f1684523b
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-08-17 03:34:27 +0000

    more documentation work.

--- java/trunk/doc/reference/build.sh
+++ java/trunk/doc/reference/build.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 xsltproc --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl sarasvati-manual.xml
+xsltproc --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl sarasvati-manual.xml
 
