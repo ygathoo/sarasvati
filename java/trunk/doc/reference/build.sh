1cdb3efb1d8bf9904869bfb74cf909cb20cc44ef
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-09-15 04:08:19 +0000

    more documentation updates

--- java/trunk/doc/reference/build.sh
+++ java/trunk/doc/reference/build.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
-xsltproc --stringparam html.stylesheet ../docbook.css --output html/ --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl src/sarasvati-manual.xml 
-xsltproc --stringparam html.stylesheet ../docbook.css --output html_single/ --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl src/sarasvati-manual.xml
+
+# Build PDF
 xsltproc --output pdf/sarasvati-manual.fo --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl src/sarasvati-manual.xml
 fop -fo pdf/sarasvati-manual.fo -pdf pdf/sarasvati-manual.pdf
+
+# Build paginated HTML
+xsltproc --stringparam html.stylesheet ../docbook.css --output html/ --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl src/sarasvati-manual.xml 
+
+# Build single page HTML
+xsltproc --stringparam html.stylesheet ../docbook.css --output html_single/ --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl src/sarasvati-manual.xml
