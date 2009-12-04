f2e49e0a7651c1bac1aa52a1f292a963f39d9e12
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-12-04 17:50:51 +0000

    Add import/export of preferences

--- java/trunk/editor/com/googlecode/sarasvati/editor/menu/ImportPreferencesAction.java
+++ java/trunk/editor/com/googlecode/sarasvati/editor/menu/ImportPreferencesAction.java
@@ -19,57 +19,46 @@
 package com.googlecode.sarasvati.editor.menu;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JFileChooser;
-import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileFilter;
 
 import com.googlecode.sarasvati.editor.GraphEditor;
-import com.googlecode.sarasvati.editor.model.Library;
+import com.googlecode.sarasvati.editor.model.EditorPreferences;
 
-public class OpenAction extends AbstractAction
+public class ImportPreferencesAction extends AbstractAction
 {
   private static final long serialVersionUID = 1L;
 
-  public OpenAction ()
+  public ImportPreferencesAction ()
   {
-    super( "Open" );
-
-    putValue( Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke( KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK ) );
-    putValue( Action.MNEMONIC_KEY, KeyEvent.VK_O );
+    super( "Import Preferences" );
+    putValue( Action.MNEMONIC_KEY, KeyEvent.VK_I );
   }
 
   @Override
-  public void actionPerformed (final ActionEvent e)
+  public void actionPerformed (final ActionEvent event)
   {
     GraphEditor editor = GraphEditor.getInstance();
 
-    File path = Library.getInstance().getBasePath();
-
-    if ( path == null )
-    {
-      path = editor.getLastFile();
-    }
-
     JFileChooser fileChooser = new JFileChooser();
-    fileChooser.setCurrentDirectory( path );
+
     fileChooser.setFileFilter( new FileFilter()
     {
       @Override
       public String getDescription ()
       {
-        return "Process Definitions";
+        return "XML Files";
       }
 
       @Override
       public boolean accept (final File f)
       {
-        return f.isDirectory() || f.getName().endsWith( ".wf.xml" );
+        return f.isDirectory() || f.getName().endsWith( ".xml" );
       }
     });
 
@@ -77,8 +66,7 @@ public class OpenAction extends AbstractAction
 
     if ( retVal == JFileChooser.APPROVE_OPTION )
     {
-      editor.setLastFile( fileChooser.getSelectedFile() );
-      GraphEditor.getInstance().openProcessDefinition( fileChooser.getSelectedFile() );
+      EditorPreferences.getInstance().importPreferences( fileChooser.getSelectedFile() );
     }
   }
 }
