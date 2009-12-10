32c62173246ebc12f57a6d0c752af667660f29df
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-12-10 20:35:14 +0000

    More join lang work

--- java/trunk/unittest/com/googlecode/sarasvati/unittest/joinlang/JoinLangCompilerTest.java
+++ java/trunk/unittest/com/googlecode/sarasvati/unittest/joinlang/JoinLangCompilerTest.java
@@ -27,10 +27,15 @@ import com.googlecode.sarasvati.join.JoinLangInterpreter;
 import com.googlecode.sarasvati.join.lang.AndJoinExpr;
 import com.googlecode.sarasvati.join.lang.JoinLangExpr;
 import com.googlecode.sarasvati.join.lang.NodeRequired;
+import com.googlecode.sarasvati.join.lang.OrJoinExpr;
+import com.googlecode.sarasvati.rubric.lang.RubricExprAnd;
+import com.googlecode.sarasvati.rubric.lang.RubricExprNot;
+import com.googlecode.sarasvati.rubric.lang.RubricExprOr;
+import com.googlecode.sarasvati.rubric.lang.RubricExprSymbol;
 
 public class JoinLangCompilerTest
 {
-  @Test public void testSimple ()
+  @Test public void testRequireNode ()
   {
     AndJoinExpr expr = new AndJoinExpr( new NodeRequired( "foo" ) );
 
@@ -39,5 +44,80 @@ public class JoinLangCompilerTest
 
     JoinLangExpr result = JoinLangInterpreter.compile( script );
     Assert.assertTrue( expr.isEqualTo( result ) );
+  }
+
+  @Test public void testRequireNodeWithWhen ()
+  {
+    NodeRequired nodeRequired = new NodeRequired( "foo" );
+    nodeRequired.setWhenExpr( new RubricExprSymbol( "Order.isExpedited" ) );
+    AndJoinExpr expr = new AndJoinExpr( nodeRequired );
+
+    String script = "require node \"foo\" when Order.isExpedited";
+    System.out.println( "SCRIPT: " + script );
+
+    JoinLangExpr result = JoinLangInterpreter.compile( script );
+    Assert.assertTrue( "Result does not match expected", expr.isEqualTo( result ) );
+  }
+
+  @Test public void testMultiRequireNode ()
+  {
+    AndJoinExpr expr = new AndJoinExpr( new NodeRequired( "foo" ) );
+    expr.add( new NodeRequired( "bar" ) );
+
+    String script = "require node \"foo\"\n" +
+                    "require node \"bar\"";
+
+    System.out.println( "SCRIPT: " + script );
+
+    JoinLangExpr result = JoinLangInterpreter.compile( script );
+    Assert.assertTrue( "Result does not match expected", expr.isEqualTo( result ) );
+  }
+
+  @Test public void testMultiRequireNodeWithWhen ()
+  {
+    NodeRequired require1 = new NodeRequired( "foo" );
+    require1.setWhenExpr( new RubricExprAnd( new RubricExprSymbol( "Order.isExpedited" ),
+                                             new RubricExprSymbol( "Order.isNew" ) ) );
+    AndJoinExpr expr = new AndJoinExpr( require1 );
+
+    NodeRequired require2 = new NodeRequired( "bar" );
+    require2.setWhenExpr( new RubricExprOr( new RubricExprNot( new RubricExprSymbol( "Order.isNew" ) ),
+                                            new RubricExprSymbol( "Order.isExpedited" ) ) );
+    expr.add( require2 );
+
+    String script = "require node \"foo\" when Order.isExpedited and Order.isNew \n" +
+                    "require node \"bar\" when not Order.isNew or Order.isExpedited";
+
+    System.out.println( "SCRIPT:\n " + script );
+
+    JoinLangExpr result = JoinLangInterpreter.compile( script );
+    System.out.println( "Expected:\n"+ expr );
+    System.out.println( "Result:\n" + result );
+    Assert.assertTrue( "Result does not match expected", expr.isEqualTo( result ) );
+  }
+
+  @Test public void testAmbiguousOr ()
+  {
+    NodeRequired require1 = new NodeRequired( "foo" );
+    require1.setWhenExpr( new RubricExprOr( new RubricExprSymbol( "Order.isExpedited" ),
+                                             new RubricExprSymbol( "Order.isNew" ) ) );
+    AndJoinExpr expr1 = new AndJoinExpr( require1 );
+
+    NodeRequired require2 = new NodeRequired( "bar" );
+    require2.setWhenExpr( new RubricExprOr( new RubricExprNot( new RubricExprSymbol( "Order.isNew" ) ),
+                                            new RubricExprSymbol( "Order.isExpedited" ) ) );
+    AndJoinExpr expr2 = new AndJoinExpr( require2 );
+    OrJoinExpr orExpr = new OrJoinExpr( expr1, expr2 );
+
+    String script = "require node \"foo\" when Order.isExpedited or Order.isNew \n" +
+                    "or\n" +
+                    "require node \"bar\" when not Order.isNew or Order.isExpedited";
+
+    System.out.println( "SCRIPT:\n " + script );
+
+    JoinLangExpr result = JoinLangInterpreter.compile( script );
+    System.out.println( "Expected:\n"+ orExpr );
+    System.out.println( "Result:\n" + result );
+    Assert.assertTrue( "Result does not match expected", orExpr.isEqualTo( result ) );
   }
 }
