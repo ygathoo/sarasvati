40c7b70ab3aba0bb6f302b816a74ee144801855e
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2009-12-31 04:13:47 +0000

    Replace jboss-j2ee jar with proper jta jar Add utilities to strip image scaling for HTML output, since scaling is really only good for PDF output.

--- java/trunk/doc/reference/build.sh
+++ java/trunk/doc/reference/build.sh
@@ -1,11 +1,21 @@
 #! /bin/sh
 
 # Build PDF
-xsltproc --output pdf/sarasvati-manual.fo --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl src/sarasvati-manual.xml
-fop -fo pdf/sarasvati-manual.fo -pdf pdf/sarasvati-manual.pdf
+#xsltproc --output pdf/sarasvati-manual.fo --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl src/sarasvati-manual.xml
+#fop -fo pdf/sarasvati-manual.fo -pdf pdf/sarasvati-manual.pdf
+
+mkdir -p classes
+
+scalac -d classes -sourcepath util/src `find util -name "*.scala"`
+
+scala -cp classes com.googlecode.sarasvati.util.doc.GraphicDescaler src/*.xml > src/sarasvati-manual-html.xml
+scala -cp classes com.googlecode.sarasvati.util.doc.ReferenceFixer src/sarasvati-manual.xml > src/sarasvati-manual-html.xml
 
 # Build paginated HTML
-xsltproc --stringparam html.stylesheet ../docbook.css --output html/ --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl src/sarasvati-manual.xml 
+xsltproc --stringparam html.stylesheet ../docbook.css --output html/ --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl src/sarasvati-manual-html.xml
 
 # Build single page HTML
-xsltproc --stringparam html.stylesheet ../docbook.css --output html_single/ --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl src/sarasvati-manual.xml
+xsltproc --stringparam html.stylesheet ../docbook.css --output html_single/ --xinclude /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl src/sarasvati-manual-html.xml
+
+rm src/*-html.xml
+rm -rf classes
