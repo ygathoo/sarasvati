2b6a90825e9d0a60e6aade28040c41c8e7caef2c
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2010-04-29 21:14:12 +0000

    Update javadoc

--- java/trunk/script/set-mime-types.sh
+++ java/trunk/script/set-mime-types.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 find doc -name "*.html" -exec svn propset "svn:mime-type" "text/html" {} \; -ls
+find doc -name "*.html" -exec svn propset "svn:eol-style" "native" {} \; -ls
 find doc -name "*.css" -exec svn propset "svn:mime-type" "text/css" {} \; -ls
+find doc -name "*.css" -exec svn propset "svn:eol-style" "native" {} \; -ls
 find doc -name "*.js" -exec svn propset "svn:mime-type" "text/javascript" {} \; -ls
+find doc -name "*.js" -exec svn propset "svn:eol-style" "native" {} \; -ls
 find doc -name "*.gif" -exec svn propset "svn:mime-type" "image/gif" {} \; -ls
