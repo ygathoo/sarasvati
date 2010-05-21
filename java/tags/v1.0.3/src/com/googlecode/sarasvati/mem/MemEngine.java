4ccfe156f17b3d62ed05eab11a0162ac70f4a2b4
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2010-05-21 04:41:55 +0000

    Tag for release

---
+++ java/tags/v1.0.3/src/com/googlecode/sarasvati/mem/MemEngine.java
@@ -0,0 +1,83 @@
+/*
+    This file is part of Sarasvati.
+
+    Sarasvati is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Sarasvati is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Sarasvati.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2008 Paul Lorenz
+*/
+
+package com.googlecode.sarasvati.mem;
+
+import com.googlecode.sarasvati.impl.BaseEngine;
+import com.googlecode.sarasvati.load.GraphLoader;
+import com.googlecode.sarasvati.load.GraphLoaderImpl;
+import com.googlecode.sarasvati.load.GraphValidator;
+
+public class MemEngine extends BaseEngine
+{
+  /**
+   * Creates a new MemEngine with the default application context.
+   * Each application context can have it's own set of global
+   * listeners.
+   */
+  public MemEngine ()
+  {
+    super( DEFAULT_APPLICATION_CONTEXT );
+  }
+
+  /**
+   * Creates a new MemEngine with the given application context.
+   * Each application context has it's own set of global listeners.
+   *
+   * This allows different applications running the same JVM to
+   * have different sets of listeners without having to add
+   * them at the process level.
+   *
+   * @param applicationContext The application context
+   */
+  public MemEngine (final String applicationContext)
+  {
+    super( applicationContext );
+  }
+
+  @Override
+  public MemGraphFactory getFactory ()
+  {
+    return MemGraphFactory.INSTANCE;
+  }
+
+  @Override
+  public MemGraphRepository getRepository ()
+  {
+    return MemGraphRepository.INSTANCE;
+  }
+
+  @Override
+  public GraphLoader<MemGraph> getLoader()
+  {
+    return getLoader( null );
+  }
+
+  @Override
+  public GraphLoader<MemGraph> getLoader (final GraphValidator validator)
+  {
+    return new GraphLoaderImpl<MemGraph>( MemGraphFactory.INSTANCE, MemGraphRepository.INSTANCE, null );
+  }
+
+  @Override
+  public MemEngine newEngine ()
+  {
+    return new MemEngine ();
+  }
+}
---
+++ java/tags/v1.0.3/src/com/googlecode/sarasvati/rubric/lang/ErrorReportingRubricLexer.java
@@ -0,0 +1,60 @@
+/*
+    This file is part of Sarasvati.
+
+    Sarasvati is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Sarasvati is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Sarasvati.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2009 Paul Lorenz
+*/
+package com.googlecode.sarasvati.rubric.lang;
+
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.RecognizerSharedState;
+
+public class ErrorReportingRubricLexer extends RubricLexer
+{
+  private ErrorReporter errorReporter;
+
+  public ErrorReportingRubricLexer (final ErrorReporter errorReporter)
+  {
+    this.errorReporter = errorReporter;
+  }
+
+  public ErrorReportingRubricLexer (final CharStream input,
+                                    final RecognizerSharedState state,
+                                    final ErrorReporter errorReporter)
+  {
+    super( input, state );
+    this.errorReporter = errorReporter;
+  }
+
+  public ErrorReportingRubricLexer (final CharStream input, final ErrorReporter errorReporter)
+  {
+    super( input );
+    this.errorReporter = errorReporter;
+  }
+
+  @Override
+  public String getErrorMessage (final RecognitionException re, final String[] arg1)
+  {
+    String message = super.getErrorMessage( re, arg1 );
+    return errorReporter.reportError( re, message );
+  }
+
+  @Override
+  public void emitErrorMessage (final String arg0)
+  {
+    // do nothing
+  }
+}
