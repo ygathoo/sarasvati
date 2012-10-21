f5798324efb64b7a65a70c7f510951e93fdc1b7b
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2012-10-21 07:26:47 +0000

    Continue improving test coverage.

---
+++ java/trunk/sarasvati-test/src/test/java/com/googlecode/sarasvati/test/execution/MultiLabelCompleteTest.java
@@ -0,0 +1,192 @@
+/*
+    This file is part of Sarasvati.
+
+    Sarasvati is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Sarasvati is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with Sarasvati.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright 2012 Paul Lorenz
+*/
+package com.googlecode.sarasvati.test.execution;
+
+import org.junit.Test;
+
+import com.googlecode.sarasvati.Arc;
+import com.googlecode.sarasvati.GraphProcess;
+import com.googlecode.sarasvati.NodeToken;
+import com.googlecode.sarasvati.test.framework.ExecutionTest;
+import com.googlecode.sarasvati.test.framework.TestProcess;
+
+public class MultiLabelCompleteTest extends ExecutionTest
+{
+  @Test
+  public void testMultiArc() throws Exception
+  {
+    GraphProcess p = startProcess("multi-arc-complete");
+
+    NodeToken token = getActiveToken( p, ":N1" );
+
+    String state = "[1 :N1 I F]";
+    TestProcess.validate( p, state );
+
+    completeToken( token, "arc1", "arc2", Arc.DEFAULT_ARC );
+
+    state =
+      "[1 :N1 C F]" +
+      "  (C F 2)" +
+      "  (C F 3)" +
+      "  (C F 4)" +
+      "[2 N2 I F]" +
+      "[3 N3 I F]" +
+      "[4 N4 I F]"
+      ;
+    TestProcess.validate( p, state );
+
+    token = getActiveToken( p, "N3" );
+
+    completeToken( token, Arc.DEFAULT_ARC );
+    state =
+        "[1 :N1 C F]" +
+        "  (C F 2)" +
+        "  (C F 3)" +
+        "  (C F 4)" +
+        "[2 N2 I F]" +
+        "[3 N3 C F]" +
+        "  (I F N5)" +
+        "[4 N4 I F]"
+        ;
+    TestProcess.validate( p, state );
+
+    token = getActiveToken( p, "N4" );
+
+    completeToken( token, Arc.DEFAULT_ARC );
+    state =
+        "[1 :N1 C F]" +
+        "  (C F 2)" +
+        "  (C F 3)" +
+        "  (C F 4)" +
+        "[2 N2 I F]" +
+        "[3 N3 C F]" +
+        "  (I F N5)" +
+        "[4 N4 C F]" +
+        "  (I F N5)"
+        ;
+    TestProcess.validate( p, state );
+
+    token = getActiveToken( p, "N2" );
+
+    completeToken( token, Arc.DEFAULT_ARC );
+    state =
+        "[1 :N1 C F]" +
+        "  (C F 2)" +
+        "  (C F 3)" +
+        "  (C F 4)" +
+        "[2 N2 C F]" +
+        "  (C F 5)" +
+        "[3 N3 C F]" +
+        "  (C F 5)" +
+        "[4 N4 C F]" +
+        "  (C F 5)" +
+        "[5 N5 I F]"
+        ;
+    TestProcess.validate( p, state );
+
+    token = getActiveToken( p, "N5" );
+
+    completeToken( token, Arc.DEFAULT_ARC );
+    state =
+        "[1 :N1 C F]" +
+        "  (C F 2)" +
+        "  (C F 3)" +
+        "  (C F 4)" +
+        "[2 N2 C F]" +
+        "  (C F 5)" +
+        "[3 N3 C F]" +
+        "  (C F 5)" +
+        "[4 N4 C F]" +
+        "  (C F 5)" +
+        "[5 N5 C F]"
+        ;
+    TestProcess.validate( p, state );
+
+    verifyComplete(p);
+  }
+
+  @Test
+  public void testMultiArc2() throws Exception
+  {
+    GraphProcess p = startProcess("multi-arc-complete2");
+
+    NodeToken token = getActiveToken( p, ":N1" );
+
+    String state = "[1 :N1 I F]";
+    TestProcess.validate( p, state );
+
+    completeToken( token, "arc1", "arc2");
+
+    state =
+      "[1 :N1 C F]" +
+      "  (C F 2)" +
+      "  (C F 3)" +
+      "[2 N2 I F]" +
+      "[3 N3 I F]"
+      ;
+    TestProcess.validate( p, state );
+
+    token = getActiveToken( p, "N2" );
+
+    completeToken( token, Arc.DEFAULT_ARC );
+    state =
+        "[1 :N1 C F]" +
+        "  (C F 2)" +
+        "  (C F 3)" +
+        "[2 N2 C F]" +
+        "  (C F 4)" +
+        "[3 N3 I F]" +
+        "[4 N5 I F]"
+        ;
+    TestProcess.validate( p, state );
+
+    token = getActiveToken( p, "N5" );
+
+    completeToken( token, Arc.DEFAULT_ARC );
+    state =
+        "[1 :N1 C F]" +
+        "  (C F 2)" +
+        "  (C F 3)" +
+        "[2 N2 C F]" +
+        "  (C F 4)" +
+        "[3 N3 I F]" +
+        "[4 N5 C F]"
+        ;
+    TestProcess.validate( p, state );
+
+    verifyIncomplete(p);
+
+    token = getActiveToken( p, "N3" );
+
+    completeToken( token, Arc.DEFAULT_ARC );
+    state =
+        "[1 :N1 C F]" +
+        "  (C F 2)" +
+        "  (C F 3)" +
+        "[2 N2 C F]" +
+        "  (C F 4)" +
+        "[3 N3 C F]" +
+        "  (C F 4)" +
+        "[4 N5 C F]"
+        ;
+    TestProcess.validate( p, state );
+
+    verifyComplete(p);
+  }
+}
---
+++ java/trunk/sarasvati-test/src/test/java/com/googlecode/sarasvati/test/suite/IntegTestSuite.java
@@ -0,0 +1,52 @@
+package com.googlecode.sarasvati.test.suite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.googlecode.sarasvati.test.backtracking.BacktrackCompletedTest;
+import com.googlecode.sarasvati.test.backtracking.BacktrackJoinTest;
+import com.googlecode.sarasvati.test.backtracking.BacktrackLinearTest;
+import com.googlecode.sarasvati.test.backtracking.BacktrackSplitJoinTest;
+import com.googlecode.sarasvati.test.backtracking.BacktrackUnevenTreesTest;
+import com.googlecode.sarasvati.test.env.TestTransientsAttributes;
+import com.googlecode.sarasvati.test.env.TokenSetMemberEnvTest;
+import com.googlecode.sarasvati.test.execution.ExternalsTest;
+import com.googlecode.sarasvati.test.execution.MultiLabelCompleteTest;
+import com.googlecode.sarasvati.test.execution.MultiStartNodeTest;
+import com.googlecode.sarasvati.test.joinlang.ArcsRequiredRequiredTest;
+import com.googlecode.sarasvati.test.joinlang.AtLeastArcsRequiredRequiredTest;
+import com.googlecode.sarasvati.test.joinlang.AtLeastLabelArcsRequiredRequiredTest;
+import com.googlecode.sarasvati.test.joinlang.LabelArcsRequiredRequiredTest;
+import com.googlecode.sarasvati.test.joinlang.NodeRequiredTest;
+import com.googlecode.sarasvati.test.joinlang.TokenSetRequiredTest;
+import com.googlecode.sarasvati.test.traversal.TraversalTest;
+
+@SuiteClasses ({
+  BacktrackCompletedTest.class,
+  BacktrackJoinTest.class,
+  BacktrackLinearTest.class,
+  BacktrackSplitJoinTest.class,
+  BacktrackUnevenTreesTest.class,
+
+  TestTransientsAttributes.class,
+  TokenSetMemberEnvTest.class,
+
+  ExternalsTest.class,
+  MultiLabelCompleteTest.class,
+  MultiStartNodeTest.class,
+
+  ArcsRequiredRequiredTest.class,
+  AtLeastArcsRequiredRequiredTest.class,
+  AtLeastLabelArcsRequiredRequiredTest.class,
+  LabelArcsRequiredRequiredTest.class,
+  NodeRequiredTest.class,
+  TokenSetRequiredTest.class,
+
+  TraversalTest.class
+})
+@RunWith(Suite.class)
+public class IntegTestSuite
+{
+  // nothing by default
+}
