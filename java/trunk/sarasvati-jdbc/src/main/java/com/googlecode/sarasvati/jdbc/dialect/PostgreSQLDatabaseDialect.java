12ab26ec9739fd49d8717060920c5a7182bd1492
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2012-11-12 03:39:36 +0000

    Move unit test suite back to sl-core Do some work on JDBC engine

--- java/trunk/sarasvati-jdbc/src/main/java/com/googlecode/sarasvati/jdbc/dialect/PostgreSQLDatabaseDialect.java
+++ java/trunk/sarasvati-jdbc/src/main/java/com/googlecode/sarasvati/jdbc/dialect/PostgreSQLDatabaseDialect.java
@@ -23,67 +23,71 @@ import com.googlecode.sarasvati.jdbc.action.ProcessInsertAction;
 public class PostgreSQLDatabaseDialect extends AbstractDatabaseDialect
 {
   private static final String GRAPH_INSERT_SQL =
-    "insert into wf_graph (name, version) values ( ?, ? ) returning id";
+    "insert into wf_graph (id, name, version) values ( nextval ('wf_graph_seq'), ?, ? ) returning id";
 
   private static final String NODE_INSERT_SQL =
-    "insert into wf_node (graph_id, name, type, guard, is_start, join_type) values ( ?, ?, ?, ?, ?, ? ) returning id";
+    "insert into wf_node (id, graph_id, name, type, guard, is_start, join_type, join_param) " +
+    " values ( nextval ('wf_node_seq'), ?, ?, ?, ?, ?, ?, ? ) returning id";
 
   private static final String NODE_REF_INSERT_SQL =
-    "insert into wf_node_ref (graph_id, node_id, instance ) values ( ?, ?, ? ) returning id";
+    "insert into wf_node_ref (id, graph_id, node_id, parent_id, external_id ) " +
+    " values ( nextval ('wf_node_ref_seq'), ?, ?, ?, ? ) returning id";
 
   private static final String ARC_INSERT_SQL =
-    "insert into wf_arc (graph_id, a_node_ref_id, z_node_ref_id, name ) values ( ?, ?, ?, ? ) returning id";
+    "insert into wf_arc (id, graph_id, a_node_ref_id, z_node_ref_id, name ) " +
+    "  values ( nextval ('wf_arc_seq'), ?, ?, ?, ? ) returning id";
 
   private static final String PROCESS_INSERT_SQL =
-    "insert into wf_process (graph_id, state, parent_token_id, create_date, version) values ( ?, 0, ?, ?, 1 ) returning id";
+    "insert into wf_process (id, graph_id, state, parent_token_id, create_date, version) " +
+    " values ( nextval ('wf_process_seq'), ?, 0, ?, ?, 1 ) returning id";
 
   private static final String NODE_TOKEN_INSERT_SQL =
-    "insert into wf_node_token (process_id, node_ref_id, attr_set_id, create_date, execution_type) " +
-    " values ( ?, ?, ?, ?, ? ) returning id";
+    "insert into wf_node_token (id, process_id, node_ref_id, attr_set_id, create_date, execution_type) " +
+    " values ( nextval ('wf_node_token_seq'), ?, ?, ?, ?, ? ) returning id";
 
   private static final String ARC_TOKEN_INSERT_SQL =
-    "insert into wf_arc_token (process_id, arc_id, parent_token_id, pending, execution_type, create_date) " +
-    " values ( ?, ?, ?, ?, ?, ? ) returning id";
+    "insert into wf_arc_token (id, process_id, arc_id, parent_token_id, pending, execution_type, create_date) " +
+    " values ( nextval ('wf_arc_token_seq'), ?, ?, ?, ?, ?, ? ) returning id";
 
 
   @Override
-  public DatabaseAction newGraphInsertAction (JdbcGraph graph)
+  public DatabaseAction newGraphInsertAction (final JdbcGraph graph)
   {
     return new GraphInsertAction( GRAPH_INSERT_SQL, graph );
   }
 
   @Override
-  public AbstractDatabaseAction newNodeInsertAction (JdbcNode node)
+  public AbstractDatabaseAction newNodeInsertAction (final JdbcNode node)
   {
     return new NodeInsertAction( NODE_INSERT_SQL, node );
   }
 
   @Override
-  public AbstractDatabaseAction newArcInsertAction (JdbcArc arc)
+  public AbstractDatabaseAction newArcInsertAction (final JdbcArc arc)
   {
     return new ArcInsertAction( ARC_INSERT_SQL, arc );
   }
 
   @Override
-  public AbstractDatabaseAction newNodeRefInsertAction (JdbcNodeRef nodeRef)
+  public AbstractDatabaseAction newNodeRefInsertAction (final JdbcNodeRef nodeRef)
   {
     return new NodeRefInsertAction( NODE_REF_INSERT_SQL, nodeRef );
   }
 
   @Override
-  public AbstractDatabaseAction newProcessInsertAction (JdbcGraphProcess process)
+  public AbstractDatabaseAction newProcessInsertAction (final JdbcGraphProcess process)
   {
     return new ProcessInsertAction( PROCESS_INSERT_SQL, process );
   }
 
   @Override
-  public AbstractDatabaseAction newArcTokenInsertAction (JdbcArcToken arcToken)
+  public AbstractDatabaseAction newArcTokenInsertAction (final JdbcArcToken arcToken)
   {
     return new ArcTokenInsertAction( ARC_TOKEN_INSERT_SQL, arcToken );
   }
 
   @Override
-  public AbstractDatabaseAction newNodeTokenInsertAction (JdbcNodeToken nodeToken)
+  public AbstractDatabaseAction newNodeTokenInsertAction (final JdbcNodeToken nodeToken)
   {
     return new NodeTokenInsertAction( NODE_TOKEN_INSERT_SQL, nodeToken );
   }
