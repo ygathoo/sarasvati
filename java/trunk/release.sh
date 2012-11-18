82b1dc0f11ce10525d01b855d0864bd9681b689c
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2012-11-18 19:45:24 +0000

    Add release script

---
+++ java/trunk/release.sh
@@ -0,0 +1,34 @@
+# Use mvn versions:set to update versions
+
+version=`cd sarasvati-parent && mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+passphrase=$1
+
+echo "Release version $version"
+
+if [[ "$version" == *SNAPSHOT* ]]
+then 
+  echo "Should not release a SNAPSHOT version! Exiting."
+  exit
+fi
+
+pushd sarasvati-parent 
+mvn clean
+mvn install 
+mvn source:jar
+mvn javadoc:jar
+popd
+
+function deployFile {
+  echo "mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=$1/pom.xml -Dfile=$1/target/$2 -Dgpg.passphrase=$3 $4"
+  mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=$1/pom.xml -Dfile=$1/target/$2 -Dgpg.passphrase=$3 $4
+}
+
+function deployModule {
+  deployFile $1 $1-$2.jar $3
+  deployFile $1 $1-$2-sources.jar $3 -Dclassifier=sources
+  deployFile $1 $1-$2-javadoc.jar $3 -Dclassifier=javadoc
+}
+
+deployModule sarasvati-core $version $passphrase
+deployModule sarasvati-hibernate $version $passphrase
+deployModule sarasvati-visual $version $passphrase
