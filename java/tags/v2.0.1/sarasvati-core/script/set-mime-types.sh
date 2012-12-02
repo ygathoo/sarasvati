884860641c81faecc8a6845b0ddb791436450815
plorenz <plorenz@2a3b5a84-814d-0410-8c37-a95beea58766>
2012-12-02 18:56:21 +0000

    Retag (as usual)

--- java/tags/v2.0.1/sarasvati-core/script/set-mime-types.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-find doc -name "*.html" -exec svn propset "svn:mime-type" "text/html" {} \; -ls
-find doc -name "*.html" -exec svn propset "svn:eol-style" "native" {} \; -ls
-find doc -name "*.css" -exec svn propset "svn:mime-type" "text/css" {} \; -ls
-find doc -name "*.css" -exec svn propset "svn:eol-style" "native" {} \; -ls
-find doc -name "*.js" -exec svn propset "svn:mime-type" "text/javascript" {} \; -ls
-find doc -name "*.js" -exec svn propset "svn:eol-style" "native" {} \; -ls
-find doc -name "*.gif" -exec svn propset "svn:mime-type" "image/gif" {} \; -ls
--- java/tags/v2.0.1/sarasvati-test/src/main/java/com/googlecode/sarasvati/test/framework/TestPredicate.java
+++
@@ -1,6 +0,0 @@
-package com.googlecode.sarasvati.test.framework;
-
-public interface TestPredicate<T>
-{
-  boolean matches(final T token);
-}
